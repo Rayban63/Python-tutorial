7cf621137eb7fbe665317af83d205fe5e6c68b15
Rayban63 <raymondvanderzanden@gmail.com>
2016-09-10 22:29:02 +0200

    Add files via upload
    wijzigingen na module 2 inclusief de opdracht

---
+++ opdracht2.py
@@ -0,0 +1,64 @@
+#initialize the vars
+firstNameMainCar = " "
+lastNameMainCar = " "
+lastNameDoctor1 = " "
+lastNameDoctor2 = " "
+germCityName = " "
+firstNameBoy1 = " "
+firstNameBoy2 = " "
+lastNameMonk1 = " "
+lastNameMonk2 = " "
+
+#userinput for vars
+firstNameMainCar = raw_input("voer hier uw voornaam in:")
+lastNameMainCar = raw_input("voer hier uw achternaam in:")
+lastNameDoctor1 = raw_input("voer achternaam dokter 1 in:")
+lastNameDoctor2 = raw_input("voer achternaam dokter 2 in:")
+germCityName = raw_input("voer Duitse plaatsnaam in:")
+#germCityName TODO find a way to accept Diacritics in user input field and present them correctly
+firstNameBoy1 = raw_input("voer voornaam voor een man in:")
+firstNameBoy2 = raw_input("voer voornaam voor een jonge in:")
+lastNameMonk1 = raw_input("voer achternaam monnik 1 in:")
+lastNameMonk2 = raw_input("voer achternaam monnik 2 in:")
+print
+print
+print
+print("Op een dag komt "+firstNameMainCar.capitalize()+" "+lastNameMainCar.capitalize()+" in het laboratorium van Dr."+lastNameDoctor1+" en Dr."+lastNameDoctor2+", dat zijn 2 vrienden van zijn vader.")
+print("Daar laat Dr."+lastNameDoctor1+", "+firstNameMainCar.capitalize()+" de materietransmitter zien. Dr."+lastNameDoctor1+" vertelt dat je hiermee in de tijd terug kan reizen.")
+print("Dat lijkt "+firstNameMainCar.capitalize()+" wel wat, en na veel zeuren mag " +firstNameMainCar.capitalize()+" in de tijdmachine.")
+print("Hij wil graag naar een riddertoernooi in Frankrijk, op 14 juni 1212.")
+print("Maar er gaat iets mis met de berekening waardoor "+firstNameMainCar.capitalize()+" op 24 juni 1212, bij de stad "+germCityName+"in Duitsland terechtkomt.")
+print
+print("Wanneer hij daar is zet hij een kruis op de steen, zodat hij weet waar hij over 4 uur weer moet zijn.")
+print("Hij ziet dat een man wordt overvallen door struikrovers en schiet hem te hulp.") 
+print("Nadat ze de struikrovers hebben verjaagd raken ze aan de praat. "+firstNameMainCar.capitalize()+" komt erachter dat de man " +firstNameBoy1.capitalize()+" heet.")
+print
+print("Na 4 uur gaat "+firstNameMainCar.capitalize()+" terug naar de steen, maar er loopt een grote stoet kinderen (hij komt er later achter")
+print("dat dit de Kinderkruistocht is) en "+firstNameMainCar.capitalize()+" kan niet door de stoet heen komen, waardoor hij te laat is.")
+print("In plaats van " +firstNameMainCar.capitalize()+", is de jongen die op de steen stond naar de 20ste eeuw geflitst.")
+print
+print("Samen met " +firstNameBoy1.capitalize()+" besloot "+firstNameMainCar.capitalize()+" om mee te lopen in de Kinderkruistocht. "+firstNameBoy2.capitalize()+", een jongen, is de leider van deze tocht.")
+print("Hij heeft (zogenaamd) van God een boodschap gekregen om deze kinderen naar Jeruzalem te brengen en die te bevrijden van de Saracenen.")
+print("Samen met Dom " +lastNameMonk1+" en Dom "+lastNameMonk2+", twee monniken, heeft hij de leiding.")
+print
+print("De Kinderkruistocht is slecht georganiseerd en "+firstNameMainCar.capitalize()+" gaat deze kruistocht regelen.")
+print("Hij maakt groepen: Jagers, vissers, vechters, verzorgers en mensen die fruit uit de bossen plukten.")
+print("Elke groep heeft zijn eigen leider. En iedereen komt op een plaats in een groep waar hij/zij goed in is.")
+print("Ze moeten van de monniken Dom " +lastNameMonk1+" en Dom " +lastNameMonk2+" snel door lopen naar Genua,")
+print("waar de zee zal wijken voor de kinderen, zodat ze naar Jeruzalem kunnen lopen, als "+firstNameBoy2.capitalize()+" zijn handen over de zee uitstrekt.")
+print("(Later blijkt dat dit een leugen is, en dat Dom " +lastNameMonk2+" en Dom "+lastNameMonk1+" "  +firstNameBoy2.capitalize()+" hebben ingefluisterd dat hij een heilige jongen is en daarom een heleboel kinderen moet lijden naar Genua, waar de zee voor hem zal wijken.")
+print("Maar de bedoeling van "+lastNameMonk1+" en "+lastNameMonk2+" is om de kinderen in Genua te verkopen als slaven.)")
+print("Onderweg maken ze van alles mee, zoals de Scharlaken Dood (een ziekte), talloze sterfgevallen, ontberingen in de Alpen, een kinderroof op de Povlakte, ziekte en nog veel meer.")
+print
+print("Dom "+lastNameMonk2+" kan het geheim niet meer voor zich houden en hij vertelt aan " +firstNameMainCar.capitalize()+" dat de zee niet zal gaan wijken")
+print(" en er schepen zullen komen voor de kinderen zodat die in Afrika als slaaf kunnen werken.") 
+print(""+firstNameMainCar.capitalize()+" vertelt dit aan de kinderen.")
+print
+print("En als dan de zee bij Genua niet wijkt voor hen en er schepen komen gaan de kinderen niet op de schepen,")
+print("maar vermoorden "+lastNameMonk1+".")
+print("De inwoners van Genua vinden niet dat de kinderen op het strand kunnen blijven en Dom "+lastNameMonk2+" gaat met een groep terug naar Keulen,")
+print("een groep blijft in Genua en een andere groep gaat met "+firstNameMainCar.capitalize()+" mee.")
+print("Een van de kinderen die met "+firstNameMainCar.capitalize()+" meegegaan is, vindt een aluminium doosje.")
+print("Het blijkt dat dit een boodschap van dr. "+lastNameDoctor1+" is.")
+print("Hierin staat dat " +firstNameMainCar.capitalize()+" over 24 uur op de plek van het gevonden doosje moet staan.")
+print("En zo wordt na 24 uur " +firstNameMainCar.capitalize()+" teruggeflitst naar zijn eigen tijd.")
---
+++ var.py
@@ -0,0 +1,17 @@
+#collect variables
+Name = raw_input("What is your name?")
+print(Name)
+Name = ("Lou")
+print(Name)
+firstName = raw_input ("What is your firstname?")
+lastName = raw_input ("What is your lastname?")
+country = raw_input ("where do you live?")
+country = (country.upper())
+print ("\nHello " + firstName + " " + lastName + "\nYou live in " + country)
+print("Op een dag komt " +firstName +" "  +lastName" in het laboratorium van Dr." )#+lastNameDoctor1 "en Dr." +lastNameDoctor2", 2 vrienden van zijn vader.")
+#string manipulation
+message = ("Hello world")
+print(message.lower())
+print(message.upper())
+print(message.swapcase())
+print(message.count("w"))
